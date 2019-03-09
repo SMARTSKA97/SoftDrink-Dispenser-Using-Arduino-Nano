c0879136c95d050f96009cf6e87787129d226c7f
S KA <30918861+SMARTSKA97@users.noreply.github.com>
2019-03-09 20:54:14 +0530

    Create i2cbasic.ino
    This is a template for I2C display.
    we need header files to be included - "Wire.h", "Adafruit_SSD1306.h" and "Adafruit_GFX.h"

---
+++ Codings/I2C/i2cbasic.ino
@@ -0,0 +1,28 @@
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+
+// OLED display TWI address
+#define OLED_ADDR   0x3C
+
+// reset pin not used on 4-pin OLED module
+Adafruit_SSD1306 display(-1);  // -1 = no reset pin
+
+// 128 x 64 pixel display
+#if (SSD1306_LCDHEIGHT != 64)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+void setup() {
+  // initialize and clear display
+  display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
+  display.clearDisplay();
+  display.display();
+
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
